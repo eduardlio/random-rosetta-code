8fd46ca6874cfb6970943e55066274084336e7ae
eduardlio <liao.edward@gmail.com>
2017-11-15 13:13:52 +1000

    Positioned everything nicer, added hover effects

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
     var tasks = [];
+    var currentTask;
     var max;
     $.get('output.json', function(data){
         data.tasks.forEach((task, i)=>{
             tasks.push(task);
         });
         max = tasks.length;
+        console.log(max);
+        setRandomTask();
     });
     $(".next").on("click", ()=>{
         setRandomTask();
     })
     function setRandomTask(){
         var random = Math.floor(Math.random()*max);
-        $("#task").html(tasks[random].title);
+        currentTask = tasks[random].title;
+        $("#task").html(currentTask);
         $(".sure").attr("href", tasks[random].link);
     }
 })
