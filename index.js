49923273f8b239fe2fc0c4056ad54b3bb52775f9
eduardlio <liao.edward@gmail.com>
2017-11-15 12:51:04 +1000

    Added more styling, a bit more functionality and firebase shit

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
+    var tasks = [];
+    var max;
     $.get('output.json', function(data){
-        console.log(data.length);
+        data.tasks.forEach((task, i)=>{
+            tasks.push(task);
+        });
     });
+    max = tasks.length;
+    $(".next").on("click", ()=>{
+        setRandomTask();
+    })
+    function setRandomTask(){
+        var random = Math.floor(Math.random()*max);
+        $("#task").html(tasks[random].title);
+        $(".sure").attr("href", tasks[random].link);
+    }
 })
